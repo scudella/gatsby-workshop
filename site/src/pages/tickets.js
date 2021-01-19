df83f75bac520603098543514c72d584217bb63b
Eduardo S. Libardi <eslibardi@gmail.com>
2021-01-19 16:17:11 -0300

    Exercise 3

---
+++ site/src/pages/tickets.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+const Ticket = ({ title = "placeholder conference" }) => (
+  <div className="text-center ">
+    <h1 className="text-5xl font-extrabold text-blue-500 leading-9 tracking-tight">
+      Grab Your Ticket To {title}
+    </h1>
+    <br />
+    <div className="flex flex-col">
+      <div className="bg-gray-300 rounded-lg m-2 p-2 h-30 align-middle hover:bg-purple-200 ">
+        <h2>Full Pass</h2>
+        <p className="text-3xl">$20</p>
+      </div>
+      <div className="bg-gray-300 rounded-lg m-2 p-2 h-30 align-middle hover:bg-purple-200 ">
+        <div>
+          <h2>VIP Pass</h2>
+          <p className="text-3xl">$45</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+function TicketsPage() {
+  return (
+    <Layout>
+      <SEO title="Tickets" keywords={[`Tickets`]} />
+      <Ticket />
+    </Layout>
+  );
+}
+
+export default TicketsPage;
