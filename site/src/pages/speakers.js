e68cac0ec895ae1054eadc19adb2ddb3bdefaee8
Eduardo S. Libardi <eslibardi@gmail.com>
2021-01-28 18:11:10 -0300

    Exercise 11

---
+++ site/src/pages/speakers.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { useStaticQuery, graphql } from "gatsby";
+import speakerCard from "../components/speakerCards";
+
+const Speaker = ({ speakers }) => (
+  <div className="text-center">
+    <h2 className="text-5xl font-extrabold text-blue-500 leading-9 tracking-tight font-inter p-4">
+      Speakers
+    </h2>
+    <speakerCard speakers={speakers} />;
+  </div>
+);
+
+export default function SpeakerPage() {
+  const data = useStaticQuery(graphql`
+    query FetchSpeakerData {
+      allSpeakersYaml {
+        edges {
+          node {
+            id
+            avatar
+            name
+            title
+            time
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+  return (
+    <Layout>
+      <SEO title="Speakers" keywords={[`Speakers`]} />
+      <Speaker speakers={data.allSpeakersYaml.edges} />
+    </Layout>
+  );
+}
